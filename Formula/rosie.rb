b400b262ce86846c3fb6cc9da0f1288449e2f639
Matthew Phillips <matthew@matthewphillips.info>
2026-01-24 10:03:13 -0500

    Initial commit: rosie formula

---
+++ Formula/rosie.rb
@@ -0,0 +1,23 @@
+class Rosie < Formula
+  desc "A robot helper for agent skills"
+  homepage "https://github.com/matthewp/rosie"
+  url "https://github.com/matthewp/rosie/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "PLACEHOLDER_SHA256"
+  license "BSD-3-Clause"
+
+  depends_on "pkg-config" => :build
+  depends_on "curl"
+  depends_on "libarchive"
+
+  def install
+    # Set PKG_CONFIG_PATH for keg-only libarchive
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["libarchive"].opt_lib/"pkgconfig"
+
+    system "make", "release"
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_match "rosie #{version}", shell_output("#{bin}/rosie --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# homebrew-rosie
+
+Homebrew tap for [rosie](https://github.com/matthewp/rosie) - a robot helper for agent skills.
+
+## Installation
+
+```bash
+brew tap matthewp/rosie
+brew install rosie
+```
+
+## Usage
+
+```bash
+rosie install owner/repo
+rosie list owner/repo
+rosie remove skill-name
+rosie agents
+```
+
+See the [main repository](https://github.com/matthewp/rosie) for full documentation.
